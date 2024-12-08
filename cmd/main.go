6864e54b2704789ef6a16ff7411a249aa7d4487d
Ruslan Demin <demin.rr@sibvaleo.com>
2024-12-08 21:51:57 +0700

    feat: proto user create

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/fatih/color"
+	user_v1 "github.com/laiker/auth/pkg/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
 )
 
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	listener, err := net.Listen("tcp", ":50052")
+
+	if err != nil {
+		panic(err)
+	}
+
+	g := grpc.NewServer()
+	reflection.Register(g)
+	user_v1.RegisterNoteV1Server(g, &listener)
+
+	log.Printf("server listening at %v", listener.Addr())
+
+	if err = g.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
+
+func CreateUser() {
+
+}
+
+func GetUser() {
+
+}
+
+func UpdateUser() {
+
+}
+
+func DeleteUser() {
+
 }
