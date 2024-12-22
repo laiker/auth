18369d6b19163b224706978e9cdb425d77109a15
Ruslan Demin <demin.rr@sibvaleo.com>
2024-12-22 20:58:40 +0700

    feat: update makefile and linters

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net"
+
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/laiker/auth/pkg/user_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"log"
-	"net"
 )
 
 type server struct {
@@ -16,26 +17,27 @@ type server struct {
 }
 
 func (s *server) Create(ctx context.Context, request *user_v1.CreateRequest) (*user_v1.CreateResponse, error) {
-	fmt.Printf("%+v", request)
+	fmt.Printf("%+v %v", request, ctx)
 	return &user_v1.CreateResponse{}, nil
 }
 
 func (s *server) Get(ctx context.Context, request *user_v1.GetRequest) (*user_v1.GetResponse, error) {
-	fmt.Printf("%+v", request)
+	fmt.Printf("%+v %v", request, ctx)
 	return &user_v1.GetResponse{}, nil
 }
 
 func (s *server) Update(ctx context.Context, request *user_v1.UpdateRequest) (*empty.Empty, error) {
-	fmt.Printf("%+v", request)
+	fmt.Printf("%+v %v", request, ctx)
 	return &empty.Empty{}, nil
 }
 
 func (s *server) Delete(ctx context.Context, request *user_v1.DeleteRequest) (*empty.Empty, error) {
-	fmt.Printf("%+v", request)
+	fmt.Printf("%+v %v", request, ctx)
 	return &empty.Empty{}, nil
 }
 
 func main() {
+	// nolint:gosec
 	listener, err := net.Listen("tcp", ":50052")
 
 	if err != nil {
@@ -51,5 +53,4 @@ func main() {
 	if err = g.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/laiker/auth
 
-go 1.23
+go 1.22
 
 require (
 	github.com/golang/protobuf v1.5.4
@@ -12,5 +12,5 @@ require (
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirectgolangci-lint run
 )
